93bf03813094dfea4cfefe73956cd0826132dc98
Paulinauwu <yadziro7682@gmail.com>
2023-10-25 01:37:50 -0600

    oferta.php

---
+++ oferta.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container" data-aos="fade-right">
+        <nav>
+            <img src="img/LOGO_INSA.png" class="logo">
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Comprar</a></li>
+                <li><a href="#">Rentar</a></li>
+                <li><a href="quienes-somos.php">Conocenos</a></li>
+                <li><a href="login.php">Iniciar Sesion</a></li>
+            </ul>
+            <button class="btn"><img src="img/icon.png">Contactanos</button>
+        </nav>
+        <div class="content">
+            <h1>Vida<br> moderna para todos</h1>
+            <p>Brindamos un servicio completo para la venta, compra o alquiler de bienes inmuebles.
+                Llevamos mas de 15 años en el mercado.
+            </p>
+                 <form>
+                    <input type="text" placeholder="📍 Ubicacion">
+                    <button type="submit" class="btn">Buscar</button>
+
+                 </form>
+        </div>
+    </div>
+</body>
+</html>
