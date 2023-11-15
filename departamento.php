4a905911793cbbe8f0ddcadf499aa46007088ab3
KevinmUwU <148278136+KevinmUwU@users.noreply.github.com>
2023-11-15 03:08:49 -0600

    Add files via upload

---
+++ departamento.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inicio de Sesión</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/styleFooter.css">
+    <link rel="stylesheet" type="text/css" href="css/departamento.css">
+</head>
+<body>
+<div class="container" data-aos="fade-right">
+        <nav>
+            <img src="img/LOGO_INSA.png" class="logo">
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="index.php">compra</a></li>
+                <li><a href="index.php">renta</a></li>
+                <li><a href="index.php">conocenos</a></li>
+
+            </ul>
+            <button class="btn"><img src="img/icon.png">Contactanos</button>
+        </nav>
+ 
+
+<div class="principal">
+<label for="principal">Departamento premium en el centro de Mexico con vistas panoramicas</label>
+</div>
+
+<div class="contenidodepartamento">
+
+<div class="deparight">
+            <img src="img/mueble.jpg" class="mueble">
+<div class="depacontent">
+<p class="circulo"><</p>
+ <img src="img/silla.jpg" class="depaimages">
+ <img src="img/cama.jpg" class="depaimages">
+ <img src="img/sala.jpg" class="depaimages">
+<p class="circulo">></p>
+</div>
+
+<div class="depacontent1">
+<center><img src="img/descripcion.jpg" class="descrip"></center>
+</div>
+<br><br>
+<center><div class="depacontent2">
+<p class="depatext1">Renta desde:ㅤㅤ</p>
+<p class="depatext">2500$ /mesㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ</p> 
+<button>Quiero rentar</button> 
+
+</div>
+</center>
+
+</div>
+
+
+
+
+<div class="depaleft">
+<center><p class="depatext">contactanos</p></center>
+<p class="depatext1">Nombre completo</p>
+ <input type="text" id="miInput" name="miInput">
+<p class="depatext1">Numero de telefono</p>
+ <input type="text" id="miInput" name="miInput">
+<p class="depatext1">E-mail</p>
+ <input type="text" id="miInput" name="miInput">
+<br><br><br><br>
+<input type="checkbox" name="tarea1"> Acepto el tratamiento de datos personales.
+<br><br>
+
+<center><button>Send Message</button> </center>
+<center><p class="depatext">Redes Sociales</p></center>
+<center><img src="img/twitter.png" width="40" height="40">
+<img src="img/fb.png" width="40" height="40">
+<img src="img/ig.png" width="40" height="40">
+<img src="img/wp.png" width="40" height="40"></center>
+</div>
+</div>
+<br><br><br><br>
+
+<div class="contenidodepartamento1">
+
+<div class="deparight1">
+<p class="depatext">Caracteristicas</p>
+<p class="depatext1">
+  => número de habitaciones: 5<br>
+  => 2do piso de 5<br>
+  => área del departamento: 223.92 m2<br>
+  => superficie de terraza: 27,09 m2<br>
+  => área del balcón: 6,63 m2<br>
+  => superficie del jardín japonés: 35 m2
+</p>
+<p class="depatext">Solucion de diseño:</p>
+<p class="depatext1">Informacion de la casa
+</p>
+<p class="depatext">Ejecución y equipamiento del apartamento:</p>
+<p class="depatext1">The apartment has intelligent control via a mobile application. Premium natural materials - wood, stone tiles, cast concrete - are found in many places in the living space.<br><br><br>
+The kitchen of the LEICHT brand with SIEMENS appliances has been made to measure, bathrooms and toilets are equipped with sanitary ware from the manufacturers VILLEROY BOCH and HANSGROHE. In the master bathroom you will find the design edition of the AXOR MASSAUD brand, the master bedroom is dominated by the RUF BETTEN bed. The living room is equipped with ROLF BENZ brand products.
+</p>
+<p class="depatext">Localizacion:</p>
+<p class="depatext1">La propiedad se encuentra encima de Devínská cesta y hay una excelente conexión de transporte. La urbanización cercana ofrece servicios cívicos completos, que incluyen tiendas, cafeterías, restaurantes, escuelas, guarderías y muchos otros beneficios
+</p>
+<p class="depatext">Opinion:</p>
+<p class="depatext1">Este hermoso apartamento ofrece un ambiente increíble. La superficie superior al estándar crea la impresión de una casa familiar, que se ve aún más realzada por la vista al río Danubio y a los bosques de su cuenca. El apartamento está amueblado con mucho gusto y es realmente atemporal. Desde nuestro punto de vista, este es uno de los apartamentos más bellos de la oferta actual: garantiza a sus propietarios un nuevo y lujoso nivel de vida.
+</p>
+</div>
+
+
+<div class="depaleft1">
+<p class="depatext">Breves caracteristicas</p>
+<p>
+  <strong>Ciudad:</strong> Barcelona l.
+</p>
+<p>
+  <strong>Calle:</strong> Vicenr ala Carne.
+</p>
+<p>
+  <strong>Estacionamiento:</strong> 2 cars.
+</p>
+<p>
+  <strong>Tipo:</strong> 5 rooms.
+</p>
+<p>
+  <strong>Número de habitáciones:</strong> 5.
+</p>
+<p>
+  <strong>Area:</strong> 224 m2.
+</p>
+<p>
+  <strong>Total area:</strong> 307 m2.
+</p>
+<p>
+  <strong>Objeto aislado:</strong> Si.
+</p>
+<p>
+  <strong>Balcón:</strong> Si.
+</p>
+<p>
+  <strong>Terraza:</strong> Si.
+</p>
+<p>
+  <strong>Número de baños:</strong> Si.
+</p>
+
+
+</div>
+</div>
+<br><br><br><br>
+
+<div class="contenedor-padre">
+    <div class="contenedor-100">
+  <img src="img/avion.png" alt="Imagen de avión" />
+  <div class="centrar-vertical">
+<br>
+    <label for="miInput">Suscribete al boletín</label>
+    <label for="miInput">recibe las ultimas novedades y ofertas interesantes e inmobiliarias</label>
+<div class="centrarinput">
+    <input type="text" id="miInput" name="miInput">
+    <button type="button">Suscribe</button>
+  </div>
+</div>
+</div>
+    <div class="contenedor-25"></div>
+  </div>
+</body>
+</html>
