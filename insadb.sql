abf6c4a0676954a38368de988534ee07ba7fa2f9
Fer7w7 <fer7w7resendiz@gmail.com>
2023-12-10 20:24:46 -0600

    Bd y ultimos cambios

--- insadb.sql
+++ insadb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 27-10-2023 a las 00:46:44
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 11-12-2023 a las 02:24:10
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,79 +24,78 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `infousuario`
+--
+
+DROP TABLE IF EXISTS `infousuario`;
+CREATE TABLE IF NOT EXISTS `infousuario` (
+  `NombreUsuario` varchar(20) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `privilegio` int NOT NULL,
+  `contraseña` varchar(30) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `activo` tinyint NOT NULL,
+  PRIMARY KEY (`NombreUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `infousuario`
+--
+
+INSERT INTO `infousuario` (`NombreUsuario`, `email`, `privilegio`, `contraseña`, `activo`) VALUES
+('Carlos', 'prueba@gmail.com', 0, '1234', 0),
+('fer', 'fer7w7@gmail.com', 1, '1234', 0),
+('fer4', 'sdffd', 2, '1234', 0),
+('fer5', 'sdffds', 3, '1234', 0),
+('feradmin', '', 1, '1234', 1),
+('ferprueba', '', 1, '1234', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `productos`
 --
 
-CREATE TABLE `productos` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `venta` varchar(10) NOT NULL,
-  `habitacion_piso` varchar(255) DEFAULT NULL,
-  `ubicacion` varchar(255) DEFAULT NULL,
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `tipoperacion` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `habitacion_piso` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `ubicacion` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `precio` decimal(10,2) NOT NULL,
-  `imagen` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `imagen` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `caracteristicas` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Opinion` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `Localizacion` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `Ejecucion_Equipamiento` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `Solucion_Disenio` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id`, `nombre`, `venta`, `habitacion_piso`, `ubicacion`, `precio`, `imagen`) VALUES
-(1, 'amplio departamento', 'si', 'hab 1', 'cancun', 12345.00, 'img/ejemplo.jpg'),
-(2, 'Ejemplo de propiedad', '1', '3 habitaciones - Piso 2', 'Ciudad Ejemplo', 200000.00, 'img/ejemplo2.jpg'),
-(3, 'gran apartamento con diseño', 'si', 'hab 4', 'tulum', 25000.00, 'img/ejemplo3.jpg'),
-(4, 'magnifico duplex en villa', 'si', 'hab 4', 'cancun', 1500.00, 'img/ejemplo4.jpg');
+INSERT INTO `productos` (`id`, `nombre`, `tipoperacion`, `habitacion_piso`, `ubicacion`, `precio`, `imagen`, `caracteristicas`, `Opinion`, `Localizacion`, `Ejecucion_Equipamiento`, `Solucion_Disenio`) VALUES
+(1, 'amplio departamento', 'venta', 'hab 1', 'cancun', '12345.00', 'img/ejemplo.jpg', NULL, '', '', '', ''),
+(2, 'Ejemplo de propiedad', 'renta', '3 habitaciones - Piso 2', 'Ciudad', '200000.00', 'img/ejemplo2.jpg', NULL, '', '', '', ''),
+(3, 'Gran apartamento con diseño', 'venta', 'hab 4', 'tulum', '25000.00', 'img/ejemplo3.jpg', '=> número de habitaciones: 5\r\n=> 2do piso de 5\r\n=> área del departamento: 223.92 m2\r\n=> superficie de terraza: 27,09 m2\r\n=> área del balcón: 6,63 m2\r\n=> superficie del jardín japonés: 35 m2', 'Este hermoso apartamento ofrece un ambiente increíble. La superficie superior al estándar crea la impresión de una casa familiar, que se ve aún más realzada por la vista al río Danubio y a los bosques de su cuenca. El apartamento está amueblado con mucho ', 'La propiedad se encuentra encima de Devínská cesta y hay una excelente conexión de transporte. La urbanización cercana ofrece servicios cívicos completos, que incluyen tiendas, cafeterías, restaurantes, escuelas, guarderías y muchos otros beneficios', 'The apartment has intelligent control via a mobile application. Premium natural materials - wood, stone tiles, cast concrete - are found in many places in the living space.\r\n\r\n\r\nThe kitchen of the LEICHT brand with SIEMENS appliances has been made to measure, bathrooms and toilets are equipped with sanitary ware from the manufacturers VILLEROY BOCH and HANSGROHE. In the master bathroom you will find the design edition of the AXOR MASSAUD brand, the master bedroom is dominated by the RUF BETTEN b', 'Informacion de la casa'),
+(4, 'magnifico duplex en villa', 'renta', 'hab 4', 'cancun', '1500.00', 'img/ejemplo4.jpg', NULL, '', '', '', '');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Estructura de tabla para la tabla `ubicacion`
 --
 
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+DROP TABLE IF EXISTS `ubicacion`;
+CREATE TABLE IF NOT EXISTS `ubicacion` (
+  `id` tinyint NOT NULL AUTO_INCREMENT,
+  `estado` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `municipio` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `CP` int NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `email`, `password`) VALUES
-(1, 'uwu@gmail.com', '12345');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- superadmin/admin.php
+++ superadmin/admin.php
@@ -350,7 +350,7 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-$sql = "SELECT nombreUsuario, activo FROM infousuario where privilegio = 1";
+$sql = "SELECT nombreUsuario, activo FROM infousuario where privilegio = 2";
 
 
 $result = $conn->query($sql);
