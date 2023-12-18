98424cff9938f9304ecedda258f9a3875e8e7e7a
Isaac Tellez <andreescalante34@gmail.com>
2023-12-18 17:14:22 -0600

    cambios en barra lateral

--- superadmin/casas.php
+++ superadmin/casas.php
@@ -29,7 +29,7 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" style="visibility:hidden">
                     <ul class="navbar-nav ml-auto navbar-right-top">
                         
                         <li class="nav-item dropdown notification">
@@ -133,9 +133,10 @@ John Abraham</h5>
         <!-- end navbar -->
         <!-- ============================================================== -->
         <!-- ============================================================== -->
-        <!-- left sidebar -->
+       <!-- ============================================================== -->
+          <!-- left sidebar -->
         <!-- ============================================================== -->
-      <div class="nav-left-sidebar sidebar-dark">
+        <div class="nav-left-sidebar sidebar-dark">
             <div class="menu-list">
                 <nav class="navbar navbar-expand-lg navbar-light">
                     <a class="d-xl-none d-lg-none" href="#">Perfil</a>
@@ -148,31 +149,31 @@ John Abraham</h5>
                                 Menu
                             </li>
                             <li class="nav-item ">
-                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
+                                <a class="nav-link " href="gerente.php" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i class="fa fa-fw fa-user-circle"></i>Dashboard <span class="badge badge-success">6</span></a>
                                 <div id="submenu-1" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
+                                       
+                                        
                                         <li class="nav-item">
-                                            <a class="nav-link" href="gerente.php"  aria-expanded="false" data-target="#submenu-1-2" aria-controls="submenu-1-2">Centro</a>
-                                            <div id="submenu-1-2"  style="">
+                                            <a class="nav-link" href="../index.php" data-toggle="" aria-expanded="false" data-target="#submenu-1-1" aria-controls="submenu-1-1">Log out</a>
+                                            <div id="submenu-1-1" class="" style="">
                                                 <ul class="nav flex-column">
                                                     
                                                 </ul>
                                             </div>
                                         </li>
-                                        
-                                       
                                     </ul>
                                 </div>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fab fa-fw fa-wpforms"></i>Propiedades</a>
+                            <li class="nav-item active">
+                                <a class="nav-link" href="casas.php" data-toggle="" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fab fa-fw fa-wpforms"></i>Propiedades</a>
                                 <div id="submenu-2" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
                                         
                                     </ul>
                                 </div>
                             </li>
-                            <li class="nav-item">
+                            <li class="nav-item" style="visibility:hidden">
                                 <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-3" aria-controls="submenu-3"><i class="fas fa-fw fa-chart-pie"></i>Catalogos</a>
                                 <div id="submenu-3" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
@@ -192,7 +193,7 @@ John Abraham</h5>
                                     </ul>
                                 </div>
                             </li>
-                            <li class="nav-item ">
+                            <li class="nav-item " style="visibility:hidden">
                                 <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-4" aria-controls="submenu-4"><i class="fab fa-fw fa-wpforms"></i>Consultas</a>
                                 <div id="submenu-4" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
@@ -208,7 +209,7 @@ John Abraham</h5>
                                     </ul>
                                 </div>
                             </li>
-                            <li class="nav-item">
+                            <li class="nav-item" style="visibility:hidden">
                                 <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-5" aria-controls="submenu-5"><i class="fas fa-fw fa-table"></i>Operativo</a>
                                 <div id="submenu-5" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
@@ -241,7 +242,7 @@ John Abraham</h5>
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                         <div class="page-header">
-                            <h2 class="pageheader-title">Gerente </h2>
+                            <h2 class="pageheader-title">Propiedades </h2>
                             <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
                             <div class="page-breadcrumb">
                                 <nav aria-label="breadcrumb">
