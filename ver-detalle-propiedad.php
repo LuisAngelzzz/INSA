90549a212cc8ef3b25cec224dd844bbb8c1c1fb4
Y. Paulina Lopez Garcia <72634495+Paulinauwu@users.noreply.github.com>
2023-12-19 10:20:26 -0600

    Update ver-detalle-propiedad.php

--- ver-detalle-propiedad.php
+++ ver-detalle-propiedad.php
@@ -61,8 +61,12 @@ function obtenerPais($id_pais)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/libs/css/style.css">
     <title>Admin</title>
 </head>
+
 
 <body>
     <?php include("header.php"); ?>
