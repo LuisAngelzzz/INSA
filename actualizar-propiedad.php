0d10e36b0418483b066c51164769fa98f60f5cfc
Y. Paulina Lopez Garcia <72634495+Paulinauwu@users.noreply.github.com>
2023-12-19 11:10:21 -0600

    Update actualizar-propiedad.php

--- actualizar-propiedad.php
+++ actualizar-propiedad.php
@@ -151,13 +151,14 @@ if (isset($_POST['actualizar'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SUBIR CASA</title>
+    <title>ADMIN</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="estilo.css">
-    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
-    <link href="assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/libs/css/style.css">
+    <link rel="stylesheet" href="superadmin/assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link href="superadmin/assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="superadmin/assets/libs/css/style.css">
     <script>
         function muestraselect(str) {
             var conexion;
