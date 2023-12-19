8015ff183999e5ab954b91fcddbcab0dabbc3b81
Y. Paulina Lopez Garcia <72634495+Paulinauwu@users.noreply.github.com>
2023-12-19 11:07:11 -0600

    Update add-propiedad.php

--- add-propiedad.php
+++ add-propiedad.php
@@ -80,7 +80,7 @@ if (isset($_POST['agregar'])) {
     <title>SUBIR CASA</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="superadmin/assets/vendor/bootstrap/css/bootstrap.min.css">
     <link href="superadmin/assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
     <link rel="stylesheet" href="superadmin/assets/libs/css/style.css">
     <link rel="stylesheet" href="superadmin/assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
